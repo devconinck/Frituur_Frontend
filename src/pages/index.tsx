c32f2cf9220a9298abc31b2dac1955a0fdd848c0
Quinten <quinten.deconinck@student.hogent.be>
2023-10-12 01:42:16 +0200

    initialise

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import { api } from "~/utils/api";
+
+export default function Home() {
+  //const { data } = api.products.getAll.useQuery();
+
+  return <></>;
+}
---
+++ src/server/api/routers/products.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const productsRouter = createTRPCRouter({
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.db.product.findMany();
+  }),
+});
