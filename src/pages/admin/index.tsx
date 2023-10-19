6c554b90bd9b910816e9e76d997e0b2c81e96985
Quinten <quinten.deconinck@student.hogent.be>
2023-10-20 00:38:32 +0200

    start admin page

---
+++ src/pages/admin/index.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from "next";
+
+const Admin: NextPage = () => {
+  return (
+    <>
+      <div>
+        <div className="flex items-center justify-center text-4xl ">
+          <p>Admin page</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Admin;
