72e7d9ae370d660cf07903ca0feaf69bf6b77028
Quinten <114664082+QuintenDeconinck@users.noreply.github.com>
2023-11-26 14:42:35 +0100

    added customer card

--- src/pages/account/customer-card/customer-card-form.tsx
+++ src/pages/account/customer-card/customer-card-form.tsx
@@ -1,3 +1,20 @@
+import { get } from "http";
+import useSWR from "swr";
+
 export default function CustomerCardForm() {
-  return <div>Here comes all the customer card settings and information</div>;
+  const { points } = useSWR("/customer-card", () => getCustomerCardById(1));
+  return (
+    <>
+      <div>Here comes all the customer card settings and information</div>
+      <div className=" h-72  w-96 rounded-lg  border-4 border-slate-500 bg-green-300 p-2">
+        <span>Frituur d'Aa</span>
+        <div className="flex grow"></div>
+        <span>Points: {points}</span>
+        <div
+          style={{ backgroundImage: `url(/Logo.png)` }}
+          className="min-w-fit-content h-32 w-32 bg-cover"
+        ></div>
+      </div>
+    </>
+  );
 }
